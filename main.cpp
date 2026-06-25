88c3e898e95f3c2eb86cfc32dbf9ba33c37b8bec
Amit Kumar <amitkumar1553p@gmail.com>
2026-06-25 18:41:17 +0000

    Implement basic HTTP server using cpp-httplib

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+#include "httplib.h"
+
+int main() {
+
+    // HTTP
+    httplib::Server svr;
+
+    svr.Get("/hi", [](const httplib::Request &, httplib::Response &res) {
+    res.set_content("Hello from C++ server!", "text/plain");
+    });
+
+    svr.listen("0.0.0.0", 8080);
+
+}
